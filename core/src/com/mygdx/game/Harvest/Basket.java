43fd2fef30ac392917618d7a238e09f2bdf2fa66
Mzamo <molimica@gmail.com>
2018-06-06 19:28:52 +0200

    assets and change un basket

--- core/src/com/mygdx/game/Harvest/Basket.java
+++ core/src/com/mygdx/game/Harvest/Basket.java
@@ -3,6 +3,7 @@ package com.mygdx.game.Harvest;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Assets;
 import com.mygdx.game.Constants;
@@ -25,7 +26,7 @@ public class Basket extends GameObject
         dimension = Constants.dimension(2,1);
         position = new Vector2(-dimension.x/2, Constants.HEIGHT_RATIO*(-4)-Constants.HEIGHT_RATIO/2);
         direction = 0;
-        basketSpeed = 500 * (worldController.level + 100);
+        basketSpeed = 400 * (worldController.level + 10);
     }
 
     @Override
@@ -34,14 +35,10 @@ public class Basket extends GameObject
     }
 
     @Override
-    public void update(float elpasedTime) {
-        if(Gdx.app.getType() == Application.ApplicationType.Android)
-        {
-            position.x = controller.longTouch().x - dimension.x / 2;
-        }
-        else {
-            position.x += direction * basketSpeed * elpasedTime;
-        }
+    public void update(float elpasedTime)
+    {
+        position.x += direction * basketSpeed * elpasedTime;
+        position.x = MathUtils.clamp(position.x, Constants.WIDTH_RATIO*-5, Constants.WIDTH_RATIO*5-dimension.x);
     }
 
     public void setDirection(float direction)
